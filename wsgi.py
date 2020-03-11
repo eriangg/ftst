c2d86d5097f561da8fae3bb61122a097775f1be2
Erik Anggard <erik.anggard@atg.se>
2020-03-11 11:13:28 +0100

    Added hostname to printed message

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from flask import Flask
 application = Flask(__name__)
+import socket
 
 @application.route("/")
 def hello():
-    return "Hejsan Världen!"
+    return "Hejsan Världen from %s!" % socket.gethostname()
 
 if __name__ == "__main__":
     application.run()
