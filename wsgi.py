3ae56c86451b0ad922c740b2e8ca39f82f688207
Erik Anggard <erik.anggard@atg.se>
2020-03-11 12:50:19 +0100

    Added timestamp

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,11 @@
 from flask import Flask
 application = Flask(__name__)
 import socket
+import datetime
 
 @application.route("/")
 def hello():
-    return "Hejsan Världen from %s!" % socket.gethostname()
+    return "Hejsan Världen from %s at %s!\n" % (socket.gethostname(), str(datetime.datetime.now()))
 
 if __name__ == "__main__":
     application.run()
